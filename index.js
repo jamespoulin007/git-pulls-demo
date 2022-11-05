55bccf57c745c2bf69e8a110e0f96e45b8c00def
James Poulin <james.poulin@tier1fin.com>
2022-11-04 20:51:57 -0400

    mostly working search ui

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
+const axios = require('axios');
 const path = require('path');
-
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,16 +13,36 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/search', (req, res) => {
+async function searchGit(gitOwner, gitRepo) {
+  const response = await axios.get(
+    `https://api.github.com/repos/${gitOwner}/${gitRepo}/pulls`
+  );
+
+  return response.data;
+}
+
+app.get('/search', async (req, res) => {
   const gitOwner = req.query.owner;
   const gitRepo = req.query.repo;
+  const searchQuery = `https://api.github.com/repos/${gitOwner}/${gitRepo}/pulls`;
   if (!gitOwner || !gitRepo) {
     res.redirect(302, '/');
     return;
   }
 
   console.log(gitOwner,"/",gitRepo);
-  res.status(200).end();
+ 
+  const results = await searchGit(gitOwner, gitRepo);
+  
+  res.render('search', {
+    title: `Pull Request Search results for: GitRepoOwner:${gitOwner} GitRepo:${gitRepo} `,
+    searchResults: results,
+    searchQuery,
+  });
+  
+  
+  // res.status(200).json(results);
+  // res.status(200).end();
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
